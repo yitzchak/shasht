a71b33b07027effe13c91f489f3e55ac7c71b179
Tarn W. Burton <twburton@gmail.com>
2020-08-24 14:37:36 -0400

    Small tweaks

--- tests/bench.lisp
+++ tests/bench.lisp
@@ -8,9 +8,9 @@
 (ql:quickload :cl-spark)
 (ql:quickload :the-cost-of-nothing)
 
-(defparameter +json+ "{\"key1\":\"value\",\"key2\":1,\"key3\":[\"Hello\",1.2e-34]}")
+(defparameter +json-string+ "{\"key1\":\"value\",\"key2\":1,\"key3\":[\"Hello\",1.2e-34]}")
 
-(defparameter +impl+
+(defparameter +json-parsers+
   (list
     'cl-json:decode-json-from-string
     'jojo:parse
@@ -21,10 +21,11 @@
     'yason:parse))
 
 (let ((benches (mapcar (lambda (fun)
-                         (the-cost-of-nothing:benchmark (funcall (symbol-function fun) +json+)))
-                       +impl+)))
-  (write-string (cl-spark:vspark benches
-                                 :min 0
-                                 :labels (mapcar (lambda (sym) (package-name (symbol-package sym))) +impl+))))
+                         (the-cost-of-nothing:benchmark (funcall (symbol-function fun) +json-string+)))
+                       +json-parsers+)))
+  (write-line (cl-spark:vspark benches
+                               :title "JSON Read Times"
+                               :min 0
+                               :labels (mapcar (lambda (sym) (package-name (symbol-package sym))) +json-parsers+))))
 
 
